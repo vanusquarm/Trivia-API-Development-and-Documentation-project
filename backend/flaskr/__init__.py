711a8eae6490069ba9a7ebc0581bf0c6dbda6d84
vanusquarm <vanusquarm@gmail.com>
2022-06-24 14:40:45 +0000

    Added custom error handlers for all expected errors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -241,7 +241,32 @@ def create_app(test_config=None):
         except:
             abort(422)
 
- 
+    """
+    @TODO:
+    Create error handlers for all expected errors
+    including 404 and 422.
+    """
+
+    # ERROR HANDLERS
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "Resource Not Found"}),
+            404
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (jsonify({"success": False, "error": 422, "message": "unprocessable"}), 422)
+    
+    @app.errorhandler(400)
+    def bad_request(error):
+        return (jsonify({"success": False, "error": 400, "message": "bad request"}), 400)
+
+    @app.errorhandler(500)
+    def bad_request(error):
+        return (jsonify({"success": False, "error": 500, "message": "Internal server error"}), 500)
+
 
     return app
 
